5e893ce93ece31ce4fddb8c1e308dcbc526e96e7
Bruno Sousa <bruno_brow7@hotmail.com>
2020-07-07 15:27:40 -0300

    - Criado DbContext para a usar o EF Core com o banco de dados SQL Server. - Configuração do Migrations para CodeFirst. - Utilização de DataAnnotations na entidade Usuario.

---
+++ APIUsuario/APIUsuario/Context/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using APIUsuario.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace APIUsuario.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        { }
+
+        public DbSet<Usuario> Usuarios { get; set; }
+    }
+}
--- APIUsuario/APIUsuario/Models/Usuario.cs
+++ APIUsuario/APIUsuario/Models/Usuario.cs
@@ -1,12 +1,25 @@
-﻿namespace APIUsuario.Models
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace APIUsuario.Models
 {
     public class Usuario
     {
         public int Id { get; set; }
+
+        [Required]
         public string Nome { get; set; }
+
+        [Required]
         public string Sobrenome { get; set; }
+
+        [Required]
         public string Email { get; set; }
-        public string DataNascimento { get; set; }
+
+        [Required]
+        public DateTime DataNascimento { get; set; }
+
+        [Required]
         public Escolaridade Escolaridade { get; set; }
     }
 
