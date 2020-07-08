cd335134b63bf4ba4fe29aa6d6711c537560203b
Bruno Sousa <bruno_brow7@hotmail.com>
2020-07-08 14:07:42 -0300

    Adicionado o Middleware CORS

--- APIUsuario/APIUsuario/Startup.cs
+++ APIUsuario/APIUsuario/Startup.cs
@@ -1,17 +1,10 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using APIUsuario.Context;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace APIUsuario
 {
@@ -46,6 +39,10 @@ namespace APIUsuario
             app.UseRouting();
 
             app.UseAuthorization();
+
+            app.UseCors(opt => opt.AllowAnyOrigin()
+                                  .AllowAnyHeader()
+                                  .AllowAnyMethod());
 
             app.UseEndpoints(endpoints =>
             {
